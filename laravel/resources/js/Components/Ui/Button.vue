9539e083d8a65f09f1cc9d179acd14ef31779248
Emilien <emilien.fuchs@ati4group.com>
2022-04-23 19:35:03 +0200

    Modified homepage animation, +2 Add
    + Modified homepage animation
    + Added Header component to dashboard
    + Added Footer, Navbar, Button, Link components

---
+++ laravel/resources/js/Components/Ui/Button.vue
@@ -0,0 +1,15 @@
+<template>
+    <button class="flex items-center hover:bg-naoka-blue-hover transition-colors inline-block bg-naoka-blue text-white py-2 px-3.5 rounded-xl text-xs sm:text-sm font-raleway font-semibold">
+        <slot/>
+    </button>
+</template>
+
+<script>
+export default {
+    name: "Button"
+}
+</script>
+
+<style scoped>
+
+</style>
